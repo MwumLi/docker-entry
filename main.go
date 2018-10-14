a3f3022ae786d2d274fafc2de338f17934a9a3cb
MwumLi <mwumli@hotmail.com>
2018-10-14 09:33:48 +0800

    extract router hanlde from main.go to router.go

--- main.go
+++ main.go
@@ -1,31 +1,16 @@
 package main
 
 import (
-	"bufio"
-	"crypto/md5"
-	"encoding/hex"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"sort"
 
-	"github.com/gorilla/websocket"
 	"github.com/julienschmidt/httprouter"
-)
-
-const (
-	port = "11245"
-	host = "127.0.0.1"
 )
 
 var (
 	Connects map[string]*Connect
 	Config   Configuration
-	upgrader = websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-	}
 )
 
 func init() {
@@ -48,155 +33,4 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	s += "POST /api/sign/exec\n"
 	s += "GET /ws/terminal/:token http -> websocket\n"
 	fmt.Fprint(w, s)
-}
-
-func wsTerminal(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	token := ps.ByName("token")
-	item, ok := Connects[token]
-	if ok == false {
-		w.WriteHeader(404)
-		fmt.Fprintf(w, "Not Found or token '%s' has expired", token)
-		return
-	}
-	// 从全局栈中删除
-	delete(Connects, token)
-
-	// websocket
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer ws.Close()
-
-	hj, err := item.Start()
-	if err != nil {
-		ws.CloseHandler()(1001, fmt.Sprintf("%s", err))
-		log.Println(err)
-		return
-	}
-	defer hj.Conn.Close()
-
-	// read data from docker and send it to websocket
-	go func() {
-		r := bufio.NewReader(hj.Br)
-		for {
-			data, err := r.ReadString('\n')
-			if err != nil {
-				break
-			}
-			ws.WriteMessage(websocket.TextMessage, []byte(data))
-			fmt.Printf("docker: %s\n", data)
-		}
-	}()
-
-	// read data from websocket and send it to docker
-	for {
-		_, data, err := ws.ReadMessage()
-		if err != nil {
-			break
-		}
-		fmt.Fprintf(hj.Conn, "%s", data)
-		fmt.Printf("websocket: %s %d\n", data, len(data))
-	}
-}
-
-func apiSignExec(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	type exec struct {
-		Container_id string
-		Host         string
-		App_key      string
-		Sign         string
-	}
-
-	type reply struct {
-		Token   string `json:"token,omitempty"`
-		Message string `json:"message,omitempty"`
-	}
-
-	responseHandle := func(statusCode int, r *reply) {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(statusCode)
-		json.NewEncoder(w).Encode(r)
-	}
-
-	var e exec
-	if err := json.NewDecoder(r.Body).Decode(&e); err != nil {
-		responseHandle(400, &reply{Message: "request body must be json"})
-		return
-	}
-
-	// 签名验证
-	if Config.Enable_sign {
-		m := make(map[string]interface{}, 0)
-		m["container_id"] = e.Container_id
-		m["host"] = e.Host
-		appKey := e.App_key
-
-		if appKey == "" || e.Sign == "" {
-			responseHandle(401, &reply{Message: "Invalid visit, miss parameter 'app_key' or 'sign'"})
-			return
-		}
-
-		m["app_key"] = appKey
-
-		appSecret, ok := Config.App_keys[appKey]
-		if ok == false {
-			responseHandle(401, &reply{Message: fmt.Sprintf("Invalid visit, app_key '%s' not exist", appKey)})
-			return
-		}
-
-		if genSignature(m, appSecret) != e.Sign {
-			responseHandle(401, &reply{Message: "Invalid visit"})
-			return
-		}
-	}
-
-	// 判断必须参数是否存在
-	if e.Container_id == "" || e.Host == "" {
-		responseHandle(400, &reply{Message: "bad parameter: miss host or container_id"})
-		return
-	}
-
-	c, err := NewConnectItemWithOpts(withNode(e.Host), withContainer(e.Container_id), withPort(Config.Docker_serve_port))
-	if err != nil {
-		responseHandle(500, &reply{Message: fmt.Sprintf("%s", err)})
-		return
-	}
-
-	execId, err := c.Exec()
-	if err != nil {
-		responseHandle(500, &reply{Message: fmt.Sprintf("%s", err)})
-		return
-	}
-
-	Connects[execId] = c // 运行时保存执行对象
-
-	log.Printf("ToTal: %d newId: %s\n", len(Connects), execId)
-	responseHandle(200, &reply{Token: execId})
-}
-
-func genSignature(params map[string]interface{}, appSecret string) (sign string) {
-
-	// key 排序
-	sortedKeys := make([]string, 0)
-	for k, _ := range params {
-		sortedKeys = append(sortedKeys, k)
-	}
-	sort.Strings(sortedKeys)
-
-	// 拼接k-v成字符串
-	var paramStr string
-	for _, k := range sortedKeys {
-		value := fmt.Sprintf("%v", params[k])
-		paramStr = paramStr + k + value
-	}
-
-	// 计算 md5 hash
-	checksum := md5.Sum([]byte(paramStr))
-	sign = hex.EncodeToString(checksum[:])
-	sign += appSecret
-	checksum = md5.Sum([]byte(sign))
-	sign = hex.EncodeToString(checksum[:])
-	return
 }
---
+++ router.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"bufio"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"sort"
+
+	"github.com/gorilla/websocket"
+	"github.com/julienschmidt/httprouter"
+)
+
+var (
+	upgrader websocket.Upgrader
+)
+
+func init() {
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+}
+
+// Signature Algorithm
+func genSignature(params map[string]interface{}, appSecret string) (sign string) {
+
+	// sort keys
+	sortedKeys := make([]string, 0)
+	for k, _ := range params {
+		sortedKeys = append(sortedKeys, k)
+	}
+	sort.Strings(sortedKeys)
+
+	// join sorted keys to string
+	var paramStr string
+	for _, k := range sortedKeys {
+		value := fmt.Sprintf("%v", params[k])
+		paramStr = paramStr + k + value
+	}
+
+	// do md5 hash
+	checksum := md5.Sum([]byte(paramStr))
+	sign = hex.EncodeToString(checksum[:])
+	sign += appSecret
+	checksum = md5.Sum([]byte(sign))
+	sign = hex.EncodeToString(checksum[:])
+	return
+}
+
+// Create an exec instance
+func apiSignExec(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	type exec struct {
+		Container_id string
+		Host         string
+		App_key      string
+		Sign         string
+	}
+
+	type reply struct {
+		Token   string `json:"token,omitempty"`
+		Message string `json:"message,omitempty"`
+	}
+
+	responseHandle := func(statusCode int, r *reply) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(statusCode)
+		json.NewEncoder(w).Encode(r)
+	}
+
+	var e exec
+	if err := json.NewDecoder(r.Body).Decode(&e); err != nil {
+		responseHandle(400, &reply{Message: "request body must be json"})
+		return
+	}
+
+	// Enable signature verification
+	if Config.Enable_sign {
+		m := make(map[string]interface{}, 0)
+		m["container_id"] = e.Container_id
+		m["host"] = e.Host
+		appKey := e.App_key
+
+		if appKey == "" || e.Sign == "" {
+			responseHandle(401, &reply{Message: "Invalid visit, miss parameter 'app_key' or 'sign'"})
+			return
+		}
+
+		m["app_key"] = appKey
+
+		appSecret, ok := Config.App_keys[appKey]
+		if ok == false {
+			responseHandle(401, &reply{Message: fmt.Sprintf("Invalid visit, app_key '%s' not exist", appKey)})
+			return
+		}
+
+		if genSignature(m, appSecret) != e.Sign {
+			responseHandle(401, &reply{Message: "Invalid visit"})
+			return
+		}
+	}
+
+	if e.Container_id == "" || e.Host == "" {
+		responseHandle(400, &reply{Message: "bad parameter: miss host or container_id"})
+		return
+	}
+
+	c, err := NewConnectItemWithOpts(withNode(e.Host), withContainer(e.Container_id), withPort(Config.Docker_serve_port))
+	if err != nil {
+		responseHandle(500, &reply{Message: fmt.Sprintf("%s", err)})
+		return
+	}
+
+	execId, err := c.Exec()
+	if err != nil {
+		responseHandle(500, &reply{Message: fmt.Sprintf("%s", err)})
+		return
+	}
+
+	// save connect instance to Connects store
+	Connects[execId] = c
+
+	log.Printf("ToTal: %d newId: %s\n", len(Connects), execId)
+	responseHandle(200, &reply{Token: execId})
+}
+
+// http upgrade to websocket and  open a communication with docker client
+func wsTerminal(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Detetermine whether the 'token' connect instance exists in the Connects store
+	// If does not exist, response 404
+	token := ps.ByName("token")
+	item, ok := Connects[token]
+	if ok == false {
+		w.WriteHeader(404)
+		fmt.Fprintf(w, "Not Found or token '%s' has expired", token)
+		return
+	}
+	// Avoid tokens being used again, so delete connect instance from Connects stor
+	delete(Connects, token)
+
+	// websocket
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer ws.Close()
+
+	hj, err := item.Start()
+	if err != nil {
+		ws.CloseHandler()(1001, fmt.Sprintf("%s", err))
+		log.Println(err)
+		return
+	}
+	defer hj.Conn.Close()
+
+	// read data from docker and send it to websocket
+	go func() {
+		r := bufio.NewReader(hj.Br)
+		for {
+			data, err := r.ReadString('\n')
+			if err != nil {
+				break
+			}
+			ws.WriteMessage(websocket.TextMessage, []byte(data))
+			fmt.Printf("docker: %s\n", data)
+		}
+	}()
+
+	// read data from websocket and send it to docker
+	for {
+		_, data, err := ws.ReadMessage()
+		if err != nil {
+			break
+		}
+		fmt.Fprintf(hj.Conn, "%s", data)
+		fmt.Printf("websocket: %s %d\n", data, len(data))
+	}
+}
